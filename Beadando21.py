fdab01f4492ea8cb4cf35cbee25608270dfd4b5d
cseanna <csendom.anna@gmail.com>
2020-04-22 14:30:14 +0200

    for ciklussal

--- Beadando21.py
+++ Beadando21.py
@@ -2,9 +2,13 @@ import math
 
 
 def abrazolasMod(n,k,r):
-    if n-r >= k :
-        return  ((r-(n-r))//2)+1
-    else:
-        return ("Nem eloallithato")
+    db=0
+    for i in range (k,r+1):
+        for j in range(k,r+1):
+            if i>j:                     #ahogy a kisebb mertekben novekvo szam nagyobb lesz mint a masik 1) duplan kezdenenk el szamolni es 2) megborulna a feladat kikotese, miszerint k<=A<=b<=B<=r
+                continue
+            if j+i == n:
+                db += 1
+    return db
 
-
+print(abrazolasMod(3,-2,2))
