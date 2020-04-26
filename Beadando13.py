f3025e3d4b5d9c498ef7c566c481790b93b81744
cseanna <63193143+cseanna@users.noreply.github.com>
2020-04-26 19:58:54 +0200

    Formazva

--- Beadando13.py
+++ Beadando13.py
@@ -1,42 +1,45 @@
 import random
 
-kitevo = random.randint(1,15)
+kitevo = random.randint(2,6)
 csapatok = list(range(1, (2 ** kitevo) + 1))
-print(csapatok)
+
 elso_kor = []
 nyertes=0
 
 def elso (csapatok):
-
+    elsosor=''
     for i in range (len(csapatok)//2):
 
         meccsek = random.sample(csapatok, k=2)
         csapatok = list(set(csapatok)-set(meccsek))
         elso_kor.append(meccsek)
-
+        elsosor+='  |{0:^4} {1:^4}|  '.format(meccsek[0],meccsek[1])
     tobbi_kor(elso_kor)
+
+    print('{0:^{x}}\n'.format(nyertes,x=150))
+
 
 def tobbi_kor(elso_kor):
     global nyertes
-    hossz=len(elso_kor)
+    hossz = len(elso_kor)
     if hossz!=1:
         osszlista=[]
-        print(elso_kor)
+        sztringsor=''
         for i in range (0,hossz,2):
             nyero1=random.choice(elso_kor[i])
             nyero2=random.choice(elso_kor[i+1])
             nyertesek=[nyero1,nyero2]
             osszlista.append(nyertesek)
-        print(osszlista)
+            sztringsor+= '  |{0:^4} {1:^4}|  '.format(nyero1,nyero2)
+        print('{0:^{x}}\n'.format(sztringsor,x=150))
+
         if len(osszlista)!=1:
             tobbi_kor(osszlista)
         else:
-
-             nyertes = random.choice(osszlista[0])
+            nyertes  = '| {0} |'.format(random.choice(osszlista[0]))
 
     else:
-
-        nyertes = random.choice(elso_kor[0])
+        nyertes = '| {0} |'.format(random.choice(osszlista[0]))
 
 elso(csapatok)
-print(nyertes)
+
