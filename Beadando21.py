3d957cde2aeb54848efc1c729e2cd3de290cd01a
cseanna <csendom.anna@gmail.com>
2020-05-08 21:13:06 +0200

    standard bemenet; kisebb, nagyobb kezelese

--- Beadando21.py
+++ Beadando21.py
@@ -1,8 +1,10 @@
-import math
+
 
 
 def abrazolasMod(n,k,r):
     db=0
+    if r<k:
+        return 'Az also hatar nem lehet nagyobb mint a felso.'
     for i in range (k,r+1):
         for j in range(k,r+1):
             if i>j:                     #ahogy a kisebb mertekben novekvo szam nagyobb lesz mint a masik 1) duplan kezdenenk el szamolni es 2) megborulna a feladat kikotese, miszerint k<=A<=b<=B<=r
@@ -11,4 +13,8 @@ def abrazolasMod(n,k,r):
                 db += 1
     return db
 
-print(abrazolasMod(3,-2,2))
+also_hatar=int(input('Add meg az also hatart: '))
+felso_hatar=int(input('Add meg a felso hatart: '))
+szam=int(input('Adj meg egy szamot: '))
+
+print(abrazolasMod(szam,also_hatar,felso_hatar))
