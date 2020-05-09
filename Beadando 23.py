966e5b5815bbfc365f397affd2ce788c13246228
cseanna <csendom.anna@gmail.com>
2020-05-09 17:26:20 +0200

    kisebb,nagyobb kezelese

--- Beadando 23.py
+++ Beadando 23.py
@@ -3,14 +3,14 @@ import numpy as np
 
 def rakovetkezo(vege,ah,fh):
     if ah > fh:
-        print('Az also hatar nem lehet nagyobb, mint a felso')
+        return 'Az also hatar nem lehet nagyobb, mint a felso.'
     vektor=np.arange(0,vege)
     for i in range(len(vektor)):
         vektor[i]=np.random.randint(ah,fh+1)
 
     rakov=0
     if len(vektor)==0 or len(vektor)==1:
-        return ' A vektorban nincsenek rakovetkezo elemek.'
+        return 'A vektorban nincsenek rakovetkezo elemek.'
     else:
         for i in range (0,len(vektor)):
             for j in range(i+1,len(vektor)):
